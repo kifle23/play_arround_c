17b3c1c83f84cae6ecea52a351846b4f485056fe
kifle23 <kiflekifle23@gmail.com>
2022-03-16 08:05:23 -0700

    first commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,13 @@
 f02ca4ae0d86:~/alx-low_level_programming/0x02-functions_nested_loops# vi 0-putchar.c
 #include "main.h"
-
 int main(void)
 {
-	char text[8] = "_putchar";
-	int i = 0;
-
-	for (i = 0; i < 8; i++)
-	{
-		_putchar(text[i]);
-	}
-	_putchar('\n');
-
-	return (0);
+char c[9 = "_putchar";
+int i = 0;
+while (i < 8)
+{
+_putchar(c[i]);
+i++;
 }
+_putchar('\n');
+return (0);
